d3e9717f568c2baeef78300f7a743f25ab43d1d2
Jake <jh14778.2014@my.bristol.ac.uk>
2015-11-07 20:19:43 +0000

    Made small change to the sample, so that it demonstrates reading a value, and craeting json with it.

--- touchtable/views.py
+++ touchtable/views.py
@@ -8,6 +8,6 @@ def index( request ):
     return HttpResponse( "Hello, World")
 
 def test( request ):
-    template = loader.get_template( 'simple.html' )
+    template = loader.get_template( 'data.json' )
     context  = RequestContext( request, { 'sometext': TestModel.objects.all()[ 0 ].name } )
     return HttpResponse( template.render( context ) )
